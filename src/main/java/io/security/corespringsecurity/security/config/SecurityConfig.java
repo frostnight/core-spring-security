80641b1ad4d4adaad2146c04320446d302636dfc
frostnight <frostnightsong@gmail.com>
2021-10-31 14:08:52 +0900

    웹 기반 인가처리 서비스 연동 AOP-PreAuthorize

--- src/main/java/io/security/corespringsecurity/security/config/SecurityConfig.java
+++ src/main/java/io/security/corespringsecurity/security/config/SecurityConfig.java
@@ -20,6 +20,7 @@ import org.springframework.security.access.vote.RoleVoter;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -48,6 +49,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
 @Slf4j
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
